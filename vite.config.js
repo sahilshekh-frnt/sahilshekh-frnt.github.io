3d4b9dfb20e0714181e406958656b3e672d3f39f
sahilshekh-frnt <savushekh541@gmail.com>
2024-07-25 17:22:41 +0530

    all doen

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/", // Set base to the repository name
   plugins: [react()],
   build: {
     outDir: "dist",
